c5e392f6662c3f6159a9636923111f310bb8d453
Rohith Sahani <rohithsahani@Rohiths-MacBook-Air.local>
2026-03-15 23:24:22 +0530

    setup forgot password

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,28 @@ import "./App.css";
 import "./index.css";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useState } from "react";
-import AdminOrders from "./AdminOrders";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 import Layout from "./components/ui/authcomponent/Auth";
+import Checkauth from "./components/commoncomponent/Checkauth";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import ResetPassword from "./ResetPassword";
+
 import Adminview from "./components/ui/adminview/Adminview";
 import AddProduct from "./AddProduct";
-import Home from "./components/Home";
-import TrackOrder from "./TrackOrder";
-import ResetPassword from "./ResetPassword";
-import ForgotPassword from "./pages/auth/ForgotPassword";
+import AdminOrders from "./AdminOrders";
 
-import Checkauth from "./components/commoncomponent/Checkauth";
-import { ToastContainer } from "react-toastify";
+import Home from "./components/Home";
+import Homemain from "./Homemain";
 import Products from "./Products";
 import Favourites from "./Favourites";
 import Cart from "./Cart";
 import Orders from "./Orders";
 import Support from "./Support";
-import Homemain from "./Homemain";
+import TrackOrder from "./TrackOrder";
 import Checkout from "./Checkout";
 
 function App() {
@@ -32,9 +34,11 @@ function App() {
   const user = JSON.parse(localStorage.getItem("loggedInUser"));
 
   return (
-    <div className="flex flex-col overflow-hidden bg-white">
+    <div className="flex flex-col min-h-screen bg-white">
       <Routes>
         <Route path="/" element={<Navigate to="/home/shop" replace />} />
+
+        {/* AUTH ROUTES */}
         <Route
           path="/auth"
           element={
@@ -46,17 +50,13 @@ function App() {
           <Route
             path="login"
             element={<Login setIsAuthenticated={setIsAuthenticated} />}
-            
           />
-          
           <Route path="register" element={<Register />} />
-          <Route path="/auth/forgot-password" element={<ForgotPassword />} />
-<Route path="/auth/reset-password/:token" element={<ResetPassword />} />
-<Route path="/auth/forgot-password" element={<ForgotPassword />} />
-
+          <Route path="forgot-password" element={<ForgotPassword />} />
+          <Route path="reset-password/:token" element={<ResetPassword />} />
         </Route>
-        
 
+        {/* ADMIN ROUTES */}
         <Route
           path="/admin"
           element={
@@ -65,13 +65,12 @@ function App() {
             </Checkauth>
           }
         >
-         
           <Route path="add-product" element={<AddProduct />} />
-           <Route path="orders" element={<AdminOrders />} />
+          <Route path="orders" element={<AdminOrders />} />
         </Route>
 
+        {/* USER ROUTES */}
         <Route path="/home" element={<Home />}>
-
           <Route path="shop" element={<Homemain />} />
           <Route path="products" element={<Products />} />
           <Route path="favourites" element={<Favourites />} />
@@ -79,38 +78,30 @@ function App() {
           <Route path="orders" element={<Orders />} />
           <Route path="support" element={<Support />} />
           <Route path="track-order/:id" element={<TrackOrder />} />
-          
-
           <Route path="checkout" element={<Checkout />} />
         </Route>
       </Routes>
-<ToastContainer
-  position="top-right"
-  autoClose={2000}
-  hideProgressBar={false}
-  closeOnClick
-  pauseOnHover
-  draggable
-  toastStyle={{
-    boxShadow: "0 6px 20px rgba(0,0,0,0.08)",
-    backgroundColor: "#ffffff",
-    color: "#000000",
-    fontWeight: "600",
-    borderRadius: "10px",
-  }}
-  progressStyle={{
-    background: "#ef8521",
-  }}
-/>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        toastStyle={{
+          boxShadow: "0 6px 20px rgba(0,0,0,0.08)",
+          backgroundColor: "#ffffff",
+          color: "#000000",
+          fontWeight: "600",
+          borderRadius: "10px",
+        }}
+        progressStyle={{
+          background: "#ef8521",
+        }}
+      />
     </div>
   );
 }
 
 export default App;
-
-
-//rzp_live_SPzUOw5TbAeiQ4
-//secret:zO2wolIijbrLfDdz1eI3daNu
-
-
-//mz!m*a2bgqMszzX01XvMlDgm&BgDmizB
